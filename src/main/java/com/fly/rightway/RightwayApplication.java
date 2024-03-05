edb85ddeddce5aea95d31e51f1ac643d2dce0618
mohammad <mohammadsherafatian@gmail.com>
2024-03-05 14:22:54 +0330

    init

---
+++ src/main/java/com/fly/rightway/RightwayApplication.java
@@ -0,0 +1,13 @@
+package com.fly.rightway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RightwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RightwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fly/rightway/RightwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fly.rightway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RightwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
